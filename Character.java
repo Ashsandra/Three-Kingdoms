f403fce33ef9fd09c812309e4d44a78755545ed1
Ashsandra <ts40@rice.edu>
2017-09-30 16:19:11 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,1 +1,52 @@
+public class Character {
+    private int hp;
+    private int maxHp;
+    private String name;
+
+    Character(int hp, int maxHp, String name){
+        this.hp = hp;
+        this.maxHp = maxHp;
+        this.name = name;
+    }
+
+    String setName() {
+        return name;
+    }
+
+    int getHp() {
+        return hp;
+    }
+
+    int getMaxHp() {
+        return maxHp;
+    }
+
+    int getLosthp() {
+        return maxHp - hp;
+    }
+
+    boolean isWounded () {
+        return getLosthp() > 0;
+        }
+
+    boolean isDead () {
+        return hp == 0;
+    }
+
+    boolean isAlive () {
+        return ! isDead();
+    }
+
+    void harm () {
+        hp --;
+        System.out.println(this.name + "受到1点伤害，体力值为" + hp);
+    }
+
+    void heal () {
+        hp ++;
+        System.out.println(this.name + "回复一点体力，体力值为" + hp);
+    }
+
+    }
+
 
