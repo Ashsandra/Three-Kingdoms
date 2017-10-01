120bac83c88cedb5b37042e08687ae15d25be2d0
Ashsandra <ts40@rice.edu>
2017-09-30 20:06:41 -0500

    rewrote the character class; changed variable blood to hp, created maxhp, etc

--- src/Player.java
+++ src/Player.java
@@ -2,15 +2,16 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Player {
-  private int handLimit;
-  private ArrayList<Card> hand;
-  private int blood;
+    private int handLimit;
+    private ArrayList<Card> hand;
+    private int hp;
 
-  Player(Character character) {
-    this.handLimit = character.getBlood();
-    this.hand = new ArrayList<Card>();
-    this.blood = character.getBlood();
-  }
+    Player(Character character) {
+        this.handLimit = character.getHp();
+        this.hand = new ArrayList<Card>();
+        this.hp = character.getHp();
+    }
 
 
 }
+
