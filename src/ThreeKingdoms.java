f5da1495fc771e217ef56cee844b978da3b17a2f
yvoliuone <yvoliuone@gmail.com>
2017-09-30 19:27:31 -0500

    Init

--- src/ThreeKingdoms.java
+++ src/ThreeKingdoms.java
@@ -7,7 +7,7 @@ public class ThreeKingdoms {
   // used to read text from the user
   private static Scanner cin = new Scanner(System.in, UTF_8.name());
 
-  public static String nextLine() {
+  public static String getAnswer() {
     return cin.nextLine();
   }
 
