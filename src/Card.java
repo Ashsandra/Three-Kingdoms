5458f53cefa658645482f85bcaa1d5a9387cecdf
yvoliuone <yvoliuone@gmail.com>
2017-12-20 14:32:24 +0800

    newer version of Card (abtract class)

--- src/Card.java
+++ src/Card.java
@@ -117,6 +117,9 @@ interface Weapon {
   int getAttackRange();
 
 
+  //TODO: 考虑把所有weapon的play()直接写在interface里 -- 把牌放入角色装备区
+
+
   // 诸葛连弩
   class ZhugeCrossbow extends Card implements Weapon {
     public ZhugeCrossbow(int rank, Suit suit) {
@@ -174,6 +177,25 @@ interface Weapon {
   }
 
 
+  // 丈八蛇矛
+  class SerpentSpear extends Card implements Weapon {
+    public SerpentSpear(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public int getAttackRange() {
+      return 3;
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
   // 贯石斧
   class RockCleavingAxe extends Card implements Weapon {
     public RockCleavingAxe(int rank, Suit suit) {
@@ -197,5 +219,182 @@ interface Weapon {
 
 // 防具牌
 interface Armor {
+
+  //TODO: 同样考虑直接一个play()写在interface里
+
+  // 八卦阵
+  class EightTrigrams extends Card implements Armor {
+    public EightTrigrams(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
+  // 仁王盾
+  class ShieldofRenWang extends Card implements Armor {
+    public ShieldofRenWang(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+}
+
+
+// 非延时类锦囊牌
+interface Tactics {
+
+  // 决斗
+  class Duel extends Card implements Tactics {
+    public Duel(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
+  // 过河拆桥
+  class BurnBridges extends Card implements Tactics {
+    public BurnBridges(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
+  // 顺手牵羊
+  class Pilfer extends Card implements Tactics {
+    public Pilfer(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
+  // 南蛮入侵
+  class BarbariansAssault extends Card implements Tactics {
+    public BarbariansAssault(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
+  // 万箭齐发
+  class HailofArrows extends Card implements Tactics {
+    public HailofArrows(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
+  // 无中生有
+  class SleightofHand extends Card implements Tactics {
+    public SleightofHand(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
+  // 无懈可击
+  class ImpeccablePlan extends Card implements Tactics {
+    public ImpeccablePlan(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+
+  // 水淹七军
+  class SevenArmies extends Card implements Tactics {
+    public SevenArmies(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+}
+
+
+// 延时类锦囊牌
+interface DelayTactics {
+
+  // 乐不思蜀
+  class Acedia extends Card implements DelayTactics {
+    public Acedia(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
+
+  // 兵粮寸断
+  class RationsDepleted extends Card implements DelayTactics {
+    public RationsDepleted(int rank, Suit suit) {
+      super(rank, suit);
+    }
+
+    @Override
+    public ThreeKingdoms play() {
+      //TODO: IMPLEMENT
+      throw new UnsupportedOperationException("The method or operation is not implemented");
+    }
+  }
 
 }
